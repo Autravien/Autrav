091648fa6643feef6b2f999dc946600dc83f069c
Autravien <160173170+Autravien@users.noreply.github.com>
2024-02-15 14:36:45 -0500

    Add files via upload

---
+++ P1LAB1_AlfordAutravien.py
@@ -0,0 +1,22 @@
+### Autravien Alford
+### 2/14/2024
+### P1LAB1
+### This program will display my name in a welcome message
+##
+### get the first name from the user and assign it to a variable
+##fname = input("Enter your first name: ")
+### get the first name from the user and assign it to a variable
+##lname = input("Enter your second name: ")
+###print out the names in a welcome message
+##print("Hello,",fname,lname,"!Welcome to CTI-110")
+##input("Press any key to exit")
+
+num1= int(input("Enter a number: "))
+
+num2= int(input("Enter a number: "))
+
+#write a mth statement with above numbers
+
+num3 = num1 + num2
+
+print(str(num1)+ "+" +str(num2) + "=" + str(num3))
